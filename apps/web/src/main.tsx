751d7bab32ddab97632d117448302f1f61ddce50
akanandhu <akananthu11@gmail.com>
2026-08-30 13:17:35 +0530

    feat: install ui libraries, expressjs

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Theme } from '@astryxdesign/core/theme'
+import { neutralTheme } from '@astryxdesign/theme-neutral/built'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme theme={neutralTheme} mode="system">
+      <App />
+    </Theme>
   </StrictMode>,
 )
