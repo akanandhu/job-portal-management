679be7ce69e54ac721d3fa57913fc08d527427cf
akanandhu <akananthu11@gmail.com>
2026-08-30 17:25:33 +0530

    feat: auth page add

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/home-page";
+import AuthPage from "./pages/auth-page";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/auth" element={<AuthPage />} />
       <Route path="*" element={<div>404 Not Found</div>} />
     </Routes>
   );
