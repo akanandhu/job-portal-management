c149567c9a1f655fb12b8cdbbb765aca0decef15
akanandhu <akananthu11@gmail.com>
2026-08-30 16:58:02 +0530

    feat: integrate react router

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,21 +1,13 @@
-import { Footer } from '@/components/layout/footer'
-import { Header } from '@/components/layout/header'
-import { AudienceCards } from '@/features/landing/components/audience-cards'
-import { FeaturedJobs } from '@/features/landing/components/featured-jobs'
-import { Hero } from '@/features/landing/components/hero'
-import { JobCategories } from '@/features/landing/components/job-categories'
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/home-page";
 
 function App() {
   return (
-    <main className="min-h-svh bg-background text-foreground">
-      <Header />
-      <Hero />
-      <FeaturedJobs />
-      <JobCategories />
-      <AudienceCards />
-      <Footer />
-    </main>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={<div>404 Not Found</div>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ apps/web/src/pages/home-page.tsx
@@ -0,0 +1,21 @@
+import { Footer } from "@/components/layout/footer";
+import { Header } from "@/components/layout/header";
+import { AudienceCards } from "@/features/landing/components/audience-cards";
+import { FeaturedJobs } from "@/features/landing/components/featured-jobs";
+import { Hero } from "@/features/landing/components/hero";
+import { JobCategories } from "@/features/landing/components/job-categories";
+
+const HomePage = () => {
+  return (
+    <main className="min-h-svh bg-background text-foreground">
+      <Header />
+      <Hero />
+      <FeaturedJobs />
+      <JobCategories />
+      <AudienceCards />
+      <Footer />
+    </main>
+  );
+}
+
+export default HomePage;
